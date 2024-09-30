ac38e2536905b1279f73747ead96c90a323e330f
Orzimurod <orzimurodyusupov2004@gmail.com>
2024-09-30 14:20:50 +0500

    'CI'

--- orders/tests.py
+++ orders/tests.py
@@ -4,4 +4,7 @@ from django.urls import reverse
 
 class LoginViewTests(TestCase):
     def test_login_view_status_code(self):
-        response = self.client.get
+        # Make a GET request to the login view
+        response = self.client.get(reverse('login'))  # Ensure 'login' is the correct URL name
+        # Assert that the response status code is 200 (OK)
+        self.assertEqual(response.status_code, 200)
