0527ca256fd195254887f2c57cf05df73b45353a
Orzimurod <orzimurodyusupov2004@gmail.com>
2024-09-30 14:15:37 +0500

    'CI'

--- orders/tests.py
+++ orders/tests.py
@@ -1,1 +1,7 @@
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+
+class LoginViewTests(TestCase):
+    def test_login_view_status_code(self):
+        response = self.client.get
